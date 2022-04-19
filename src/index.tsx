b9edfa894498e352cac67e03ccff2b86f31f39ff
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-04-19 19:06:31 -0400

    execution using SrcDov

--- src/index.tsx
+++ src/index.tsx
@@ -35,16 +35,14 @@ const App = () => {
 			},
 		});
 
-		// console.log(result);
-
 		setCode(result.outputFiles[0].text);
-
-		try {
-			eval(result.outputFiles[0].text);
-		} catch (err) {
-			alert(err);
-		}
 	};
+
+	const html = `
+    <script>
+      ${code}
+    </script>
+  `;
 
 	return (
 		<div>
@@ -56,7 +54,7 @@ const App = () => {
 				<button onClick={onClick}>Submit</button>
 			</div>
 			<pre>{code}</pre>
-			<iframe sandbox="allow-same-origin" src="/test.html" />
+			<iframe sandbox="allow-scripts" srcDoc={html} />
 		</div>
 	);
 };
