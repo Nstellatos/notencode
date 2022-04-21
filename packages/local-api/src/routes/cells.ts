d19f510deac6a656774b9173735137b1372e7a07
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-04-21 17:27:10 -0400

    default cell values

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -3,34 +3,42 @@ import fs from 'fs/promises';
 import path from 'path';
 
 interface Cell {
-  id: string;
-  content: string;
-  type: 'text' | 'code';
+	id: string;
+	content: string;
+	type: 'text' | 'code';
 }
 
 export const createCellsRouter = (filename: string, dir: string) => {
-  const router = express.Router();
+	const router = express.Router();
 
-  const fullPath = path.join(dir, filename);
+	const fullPath = path.join(dir, filename);
 
-  router.get('/cells', async (req, res) => {
-    // Make sure the cell storage file exists
-    // If it does not exist, add in a default list of cells
-    // Read the file
-    // Parse a list of cells out of it
-    // Send list of cells back to browser
-  });
+	router.get('/cells', async (req, res) => {
+		try {
+			// Read the file
+			const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
 
-  router.post('/cells', async (req, res) => {
-    // Take the list of cells from the request obj
-    // serialize them
-    const { cells }: { cells: Cell[] } = req.body;
+			res.send(JSON.parse(result));
+		} catch (err: any) {
+			if (err.code === 'ENOENT') {
+				await fs.writeFile(fullPath, '[]', 'utf-8');
+				res.send([]);
+			} else {
+				throw err;
+			}
+		}
+	});
 
-    // Write the cells into the file
-    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+	router.post('/cells', async (req, res) => {
+		// Take the list of cells from the request obj
+		// serialize them
+		const { cells }: { cells: Cell[] } = req.body;
 
-    res.send({ status: 'ok' });
-  });
+		// Write the cells into the file
+		await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
 
-  return router;
+		res.send({ status: 'ok' });
+	});
+
+	return router;
 };
